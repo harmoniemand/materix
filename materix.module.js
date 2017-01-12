4439245a0373bc0f01c46ed16171cdf9b6904d59
harmoniemand <jonathan.guenz@outlook.com>
2017-01-13 00:17:45 +0100

    materix initial

---
+++ materix.module.js
@@ -0,0 +1,34 @@
+
+module.exports = function(options) {
+
+    var _sampleBox = {
+        orientation: landscape, // portrait
+        
+    }
+
+    var boxes = [];
+
+
+    function send() {
+
+    };
+
+    function setPixel() {
+
+    };
+
+    function setList() {
+
+    };
+
+    function setArray() {
+
+    };
+
+    return {
+        send: send,
+        setPixel: setPixel,
+        setList: setList,
+        setArray: setArray
+    };
+};
