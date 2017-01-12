10c0f4848b106a785b8e40c781de0da46e6fca8e
harmoniemand <jonathan.guenz@outlook.com>
2017-01-12 22:23:06 +0100

    test

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 var SerialPort = require('serialport');
 var port = new SerialPort('/dev/serial0');
 
-port.on('open', function() {
-  port.write('main screen turn on', function(err) {
-    if (err) {
-      return console.log('Error on write: ', err.message);
+var write = function () {
+    for (var i = 0; i < 240; i++) {
+        port.write(100, function (err) {
+            if (err) {
+                return console.log('Error on write: ', err.message);
+            }
+            console.log('message written');
+        });
     }
-    console.log('message written');
-  });
+
+    setTimeout(write, 20);    
+};
+
+
+port.on('open', function () {
+    setTimeout(write, 20);
 });
 
 // open errors will be emitted as an error event
-port.on('error', function(err) {
-  console.log('Error: ', err.message);
+port.on('error', function (err) {
+    console.log('Error: ', err.message);
 })
