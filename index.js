4075ae2541017a9191343e07c218e39bbffac242
harmoniemand <jonathan.guenz@outlook.com>
2017-01-12 21:07:44 +0100

    bla

---
+++ index.js
@@ -0,0 +1,16 @@
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/tty-usbserial1');
+
+port.on('open', function() {
+  port.write('main screen turn on', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+    console.log('message written');
+  });
+});
+
+// open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
